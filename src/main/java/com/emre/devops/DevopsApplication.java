824c2f6d0391aac81be0dd9a7deba63014ea2564
emreTurkeri <112562188+emreTurkeri@users.noreply.github.com>
2025-07-04 18:06:20 +0300

    add .war

--- src/main/java/com/emre/devops/DevopsApplication.java
+++ src/main/java/com/emre/devops/DevopsApplication.java
@@ -2,14 +2,22 @@ package com.emre.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class DevopsApplication {
+public class DevopsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DevopsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsApplication.class, args);
 	}
 
 }
+
